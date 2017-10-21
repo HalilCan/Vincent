428c1f4e4a496b8fe288630b88fc3333153f7ccb
HalilCan <halilcanmemoglu@gmail.com>
2017-10-20 21:16:01 -0400

    Redoing introductory C++ after a decade is not that much fun.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+void DoArea(void);
 
 int main() {
     int num, answer, temp, value;
@@ -10,5 +13,17 @@ int main() {
 
     cout << "Hello, Hal!" << endl;
     std::cout << "Hello, World!" << std::endl;
+
+    cout << "The square root of 1.44 is: " << sqrt(1.44) << endl;
+
+    DoArea();
+
     return 0;
+}
+
+void DoArea() {
+    float length, width;
+    length = 10.3f;
+    width = 5.2f;
+    cout << "The Area of a rectangle with dimensions 5.2 and 10.3 is: " << width*length << endl;
 }
